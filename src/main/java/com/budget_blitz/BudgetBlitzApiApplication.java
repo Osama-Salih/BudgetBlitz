931c26359fa69cd754e90575a136b6707599cfe8
Osama Salih <osamasalih.ca@gmail.com>
2025-10-23 11:03:52 +0300

    Project configration

---
+++ src/main/java/com/budget_blitz/BudgetBlitzApiApplication.java
@@ -0,0 +1,13 @@
+package com.budget_blitz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetBlitzApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetBlitzApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/budget_blitz/BudetBlitzApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budget_blitz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudetBlitzApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
